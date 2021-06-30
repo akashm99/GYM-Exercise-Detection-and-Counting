51ee8892da7ab066a771bc221614004a823db99a
akashm99 <akashbm08@gmail.com>
2021-06-30 19:00:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# GYM-Exercise-Detection-and-Counting" 
---
+++ gym_app.py
@@ -0,0 +1,150 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+from tensorflow.keras.models import load_model
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks,
+                              mp_holistic.FACE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1))
+    mp_drawing.draw_landmarks(image, results.pose_landmarks,
+                              mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks,
+                              mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks,
+                              mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
+
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(63)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(63)
+    return np.concatenate([pose, face, lh, rh])
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    c = np.array(c)  # End
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+
+    return output_frame
+
+
+actions = np.array(['B-Curls', 'OH-Press', 'Neutral'])
+model = load_model('gym.h5')
+
+colors = [(245, 117, 16), (117, 245, 16), (16, 117, 245)]
+
+sequence = []
+predictions = []
+threshold = 0.7
+
+curls = 0
+press = 0
+
+cap = cv2.VideoCapture("1.mp4")
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        try:
+            image, results = mediapipe_detection(frame, holistic)
+
+            draw_styled_landmarks(image, results)
+
+            keypoints = extract_keypoints(results)
+            sequence.append(keypoints)
+            sequence = sequence[-30:]
+        except:
+            pass
+
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            predictions.append(np.argmax(res))
+
+            if np.unique(predictions[-10:])[0] == np.argmax(res):
+                if res[np.argmax(res)] > threshold:
+                    try:
+                        landmarks = results.pose_landmarks.landmark
+                        shoulder = [landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].x,
+                                    landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].y]
+                        elbow = [landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].x,
+                                 landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y]
+                        wrist = [landmarks[mp_holistic.PoseLandmark.LEFT_WRIST.value].x,
+                                 landmarks[mp_holistic.PoseLandmark.LEFT_WRIST.value].y]
+                        angle = calculate_angle(shoulder, elbow, wrist)
+                        if actions[np.argmax(res)] == "B-Curls" or actions[np.argmax(res)] == "Neutral":
+                            if angle > 150:
+                                stage_c = "down"
+                            if angle < 60 and stage_c == 'down':
+                                stage_c = "up"
+                                curls += 1
+                                print(f"curls: {curls}")
+                        elif actions[np.argmax(res)] == "OH-Press" or actions[np.argmax(res)] == "Neutral":
+                            if angle < 90:
+                                stage_o = "down"
+                            if angle > 100 and stage_o == 'down':
+                                stage_o = "up"
+                                press += 1
+                                print(f"press: {press}")
+                    except:
+                        pass
+
+            image = prob_viz(res, actions, image, colors)
+
+        cv2.rectangle(image, (0, 0), (640, 40), (255, 0, 0), -1)
+        cv2.putText(image, f"REPS: curls: {curls} | press: {press}", (3, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.imshow('OpenCV Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
